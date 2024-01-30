501fd0d2224d44bf577c99556f1e3bd6ea103e45
bemadeveloper <akbermet1990@icloud.com>
2024-01-30 20:40:50 +0600

    Initial Commit

---
+++ HomeWork11/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork11
+//
+//  Created by Bema on 30/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
